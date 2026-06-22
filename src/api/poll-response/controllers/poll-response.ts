dfb40cb5f14209c5851aedb813bd07e637fc6b08
johnathantam00 <84328415+johnathantam@users.noreply.github.com>
2026-06-21 20:10:17 -0400

    Changed back to individual responses

--- src/api/poll-response/controllers/poll-response.ts
+++
@@ -1,7 +0,0 @@
-/**
- * poll-response controller
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreController('api::poll-response.poll-response');
---
+++ src/api/slider-poll-response/controllers/slider-poll-response.ts
@@ -0,0 +1,7 @@
+/**
+ * slider-poll-response controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::slider-poll-response.slider-poll-response');
